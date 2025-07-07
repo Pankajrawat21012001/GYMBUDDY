282af49ba6e0032cf9789da9771b16305a1594a7
Pankajrawat21012001 <rawatpankaj9876@gmail.com>
2025-07-07 15:12:21 +0530

    second commit:- removing server from vite config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base:process.env.VITE_BASE_PATH || '/GYMBUDDY',
-  server: {
-        proxy: {
-          '/api': {
-            target: 'http://localhost:3000'
-          },
-        },
-      },
+  base:process.env.VITE_BASE_PATH || '/GYMBUDDY'
 })
